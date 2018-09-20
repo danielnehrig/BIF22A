3cf863177375779d0d84dcaea9b8d388cab59339
Daniel Nehrig <daniel.nehrig@redteclab.com>
2018-09-20 12:28:44 +0200

    cow update

--- src/MilchErfassung/test.cs
+++ src/MilchErfassung/test.cs
@@ -1,32 +1,59 @@
 using System;
+using System.Collections.Generic;
 
 public class Cow {
-  private string _kuhId;
-  public string KuhId {
-    get { return _kuhId; }
-    set { _kuhId = value; }
+  private int _cowId;
+  public int cowId {
+    get { return _cowId; }
+    set { _cowId = value; }
   }
 
-  private double _milchkapazitaet;
-  public double Milchkapazitaet {
-    get { return _milchkapazitaet; }
-    set { _milchkapazitaet = value; }
+  private Euter _euter;
+  public Euter euter {
+    get { return _euter; }
+    //set { _euter = value; }
   }
 
-  public Cow(string id, double MilchStart) {
-    _kuhId = id;
-    _milchkapazitaet = MilchStart;
+  public Cow(int id, double milkstart) {
+    _cowId = id;
+    _euter = new Euter(milkstart);
   }
-  public double Milchgeben(double Milch) {
-    _milchkapazitaet -= Milch;
-    return _milchkapazitaet;
+}
+
+public class Euter {
+  private double _milkvolume;
+  public double Milkvolume {
+    get { return _milkvolume; }
+    set { _milkvolume = value; }
+  }
+  public Euter(double milkstart) {
+    _milkvolume = milkstart;
+  }
+  public double giveMilk(double Milk) {
+    _milkvolume -= Milk;
+    return _milkvolume;
+  }
+}
+
+public class Stall {
+  private List<Cow> _place = new List<Cow>();
+  public List<Cow> place {
+    get { return _place; }
+    set { _place = value; }
+  }
+  public void addToList(Cow cow) {
+    _place.Add(cow);
   }
 }
 
 class Example {
   static void Main() {
-    Cow name = new Cow("19392", 32.02);
-    Console.WriteLine(name.Milchgeben(10));
-    Console.WriteLine(Math.random());
+    int amount = 5;
+    Stall stall = new Stall();
+    for (int i=0; i < amount; i++) {
+      Cow name = new Cow(i, 32.02);
+      stall.addToList(name);
+    }
+    Console.WriteLine(stall.place[0].euter.giveMilk(10));
   }
 }
